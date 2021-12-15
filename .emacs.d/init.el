9e4608294dd50777916243636b3ef7116fcdc417
Oliver Baptiste <github@oliverbaptiste.com>
2021-12-15 11:57:52 -0500

    Commenting out web-mode indent offsets. Need more elegant solution here.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -89,14 +89,15 @@
    )
   :config
   (setq
-   web-mode-markup-indent-offset 2
-   web-mode-css-indent-offset 2
-   web-mode-code-indent-offset 2
-   web-mode-attr-indent-offset 2
+;; web-mode-markup-indent-offset 2
+;; web-mode-css-indent-offset 2
+;; web-mode-code-indent-offset 2
+;; web-mode-attr-indent-offset 2
    web-mode-style-padding 0
    web-mode-script-padding 0
    web-mode-offsetless-elements '("html" "head" "body" "script")
-   web-mode-attr-indent-offset 2))
+;; web-mode-attr-indent-offset 2
+   ))
 
 (use-package markdown-mode
   :ensure t)
