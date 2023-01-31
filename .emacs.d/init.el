0f3e60e2d8e9d19bab9ca4c8f2b9c63e6e510635
Oliver Baptiste <ob@oliverbaptiste.com>
2023-01-31 14:47:37 -0500

    2023-01-31 update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,71 +1,30 @@
-;; Default typeface
-(set-face-attribute 'default nil
-                    :family "SF Mono"
-                    :height 160
-                    :weight 'normal
-                    :width 'normal)
-
-;; Mac keybindings
-(cond ((eq system-type 'darwin)
-       (setq mac-command-modifier 'super
-             mac-option-modifier 'meta)
-       (global-set-key (kbd "s--") (kbd "C-x C--")) ;; Decrease font size
-       (global-set-key (kbd "s-=") (kbd "C-x C-=")) ;; Increase font size
-       (global-set-key (kbd "s-0") (kbd "C-x C-0")) ;; Reset font size
-       (global-set-key (kbd "s-z") (kbd "C-/")) ;; Undo
-       (global-set-key (kbd "s-x") (kbd "C-w")) ;; Cut
-       (global-set-key (kbd "s-c") (kbd "M-w")) ;; Copy
-       (global-set-key (kbd "s-v") (kbd "C-y")) ;; Paste
-       (global-set-key (kbd "s-a") (kbd "C-x h")) ;; Select All
-       (global-set-key (kbd "s-s") (kbd "C-x C-s")) ;; Save
-       ))
-
-;; Scratch buffer defaults to org-mode with no message
-(setq initial-major-mode 'org-mode)
-(setq initial-scratch-message "")
-
-;; Use MELPA for package archive
-(package-initialize)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/"))
-
-;; Make sure that use-package is loaded
-(when (not (package-installed-p 'use-package))
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; Set up default fonts
+(cond
+ ((find-font (font-spec :name "Fira Mono"))
+  (set-frame-font "Fira Mono-14"))
+ ((find-font (font-spec :name "Source Code Pro"))
+  (set-frame-font "Source Code Pro-14"))
+ ((find-font (font-spec :name "SF Mono"))
+  (set-frame-font "SF Mono-14"))
+ ((find-font (font-spec :name "Menlo"))
+  (set-frame-font "Menlo")))
 
 ;; Move all custom-* stuff into custom-file.el.
 ;; Do not put them in init.el.
 (setq custom-file "~/.emacs.d/custom-file.el")
 
 ;; Prevent autosave mess
-;; Source: https://www.victorquinn.com/emacs-prevent-autosave-mess
+;; Sources:
+;; https://www.victorquinn.com/emacs-prevent-autosave-mess
+;; https://snarfed.org/gnu_emacs_backup_files
+(make-directory "~/.emacs.d/autosaves/" t)
 (setq auto-save-file-name-transforms
       `((".*" "~/.emacs.d/autosaves/\\1" t)))
 (setq backup-directory-alist
       `((".*" . "~/.emacs.d/backups/")))
 
-;; Load themes
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-
-;; Load Dracula theme
-(load-theme 'dracula t)
-
-;; Enable Dracula theme for GUI Emacs only
-(if (display-graphic-p)
-    (enable-theme 'dracula)
-  (disable-theme 'dracula))
-
-;; Do not show the startup screen
+;; Don't show the startup screen
 (setq inhibit-startup-message t)
-
-;; Disable tool bar
-(tool-bar-mode -1)
-
-;; Disable menu bar in non-GUI Emacs
-;; Source: https://emacs.stackexchange.com/questions/29441/how-do-i-disable-menu-bar-mode-only-for-tty-frames
-(unless (display-graphic-p)
-  (menu-bar-mode -1))
 
 ;; Highlight current line
 (global-hl-line-mode t)
@@ -77,76 +36,44 @@
 ;; Add line numbers in programming modes
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
-;; START TABS CONFIG
-;; Source: https://dougie.io/emacs/indentation/
+;; Emacs package management with straight.el and use-package
+;; Source: https://jeffkreeftmeijer.com/emacs-straight-use-package/
 
-;; Create a variable for our preferred tab width
-(setq custom-tab-width 2)
+;; Install straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; Two callable functions for enabling/disabling tabs in Emacs
-(defun disable-tabs ()
-  (setq indent-tabs-mode nil)
-  (setq tab-width custom-tab-width))
-(defun enable-tabs ()
-  (local-set-key (kbd "TAB") 'tab-to-tab-stop)
-  (setq indent-tabs-mode t)
-  (setq tab-width custom-tab-width))
+;; Install use-package
+(straight-use-package 'use-package)
 
-;; Hooks to Disable Tabs in programming modes
-(add-hook 'prog-mode-hook 'disable-tabs)
+;; Configure use-package to use straight.el by default
+(use-package straight
+  :custom (straight-use-package-by-default t))
 
-;; Disable electric-indent in org-mode
-(add-hook 'org-mode-hook
-	  (lambda () (electric-indent-local-mode -1)))
-
-;; Making electric-indent behave sanely
-(setq-default electric-indent-inhibit t)
-
-;; Make the backspace properly erase the tab instead of
-;; removing 1 space at a time.
-(setq backward-delete-char-untabify-method 'hungry)
-
-;; Do not indent source blocks in org-mode
-(setq org-edit-src-content-indentation 0)
-
-;; PACKAGES
-(use-package elpher
-  :ensure t)
-
-(use-package groovy-mode
-  :ensure t)
-
-(use-package magit
-  :ensure t)
-
-(use-package markdown-mode
-  :ensure t)
-
+;; Install packages
 (use-package edit-indirect
-  :ensure t)
-
-(use-package php-mode
-  :ensure t)
-
-(use-package racket-mode
-  :ensure t)
-
-(use-package sml-mode
-  :ensure t)
-
-(use-package web-mode
-  :ensure t
-  :mode
-  (
-   ".html?$"
-   )
+  :straight t)
+(use-package editorconfig
+  :straight t
   :config
-  (setq
-   web-mode-attr-indent-offset custom-tab-width
-   web-mode-css-indent-offset custom-tab-width
-   web-mode-code-indent-offset custom-tab-width
-   web-mode-markup-indent-offset custom-tab-width
-   web-mode-offsetless-elements '("html" "head" "body" "script")
-   web-mode-script-padding 0
-   web-mode-style-padding 0
-   ))
+  (editorconfig-mode 1))
+(use-package elpher
+  :straight t)
+(use-package groovy-mode
+  :straight t)
+(use-package magit
+  :straight t)
+(use-package markdown-mode
+  :straight t)
+(use-package web-mode
+  :straight t)
