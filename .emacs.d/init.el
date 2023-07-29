1e6347b29fba094db0b1c8ec2b8ccf9568dd0c51
Oliver Baptiste <ob@oliverbaptiste.com>
2023-07-28 21:43:52 -0400

    Might be filing for .emacs bankrupcy soon...
    Stripping out fonts and package management

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,13 +1,5 @@
-;; Set up default fonts
-(cond
- ((find-font (font-spec :name "Fira Mono"))
-  (set-frame-font "Fira Mono-14"))
- ((find-font (font-spec :name "Source Code Pro"))
-  (set-frame-font "Source Code Pro-14"))
- ((find-font (font-spec :name "SF Mono"))
-  (set-frame-font "SF Mono-14"))
- ((find-font (font-spec :name "Menlo"))
-  (set-frame-font "Menlo")))
+;; Enable a color theme
+(load-theme 'modus-operandi)
 
 ;; Move all custom-* stuff into custom-file.el.
 ;; Do not put them in init.el.
@@ -26,6 +18,9 @@
 ;; Don't show the startup screen
 (setq inhibit-startup-message t)
 
+;; Disable tool bar
+(tool-bar-mode -1)
+
 ;; Highlight current line
 (global-hl-line-mode t)
 
@@ -35,45 +30,3 @@
 
 ;; Add line numbers in programming modes
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
-
-;; Emacs package management with straight.el and use-package
-;; Source: https://jeffkreeftmeijer.com/emacs-straight-use-package/
-
-;; Install straight.el
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-;; Install use-package
-(straight-use-package 'use-package)
-
-;; Configure use-package to use straight.el by default
-(use-package straight
-  :custom (straight-use-package-by-default t))
-
-;; Install packages
-(use-package edit-indirect
-  :straight t)
-(use-package editorconfig
-  :straight t
-  :config
-  (editorconfig-mode 1))
-(use-package elpher
-  :straight t)
-(use-package groovy-mode
-  :straight t)
-(use-package magit
-  :straight t)
-(use-package markdown-mode
-  :straight t)
-(use-package web-mode
-  :straight t)
