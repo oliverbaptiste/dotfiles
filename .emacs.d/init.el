3b8566e16b405e34c912bf71cfbbc84a693b2380
Oliver Baptiste <ob@oliverbaptiste.com>
2023-07-30 15:44:00 -0400

    Update init.el with simplified package management

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,19 +1,17 @@
-;; Enable a color theme
-(load-theme 'modus-operandi)
-
 ;; Move all custom-* stuff into custom-file.el.
 ;; Do not put them in init.el.
 (setq custom-file "~/.emacs.d/custom-file.el")
 
 ;; Prevent autosave mess
-;; Sources:
-;; https://www.victorquinn.com/emacs-prevent-autosave-mess
-;; https://snarfed.org/gnu_emacs_backup_files
 (make-directory "~/.emacs.d/autosaves/" t)
 (setq auto-save-file-name-transforms
       `((".*" "~/.emacs.d/autosaves/\\1" t)))
 (setq backup-directory-alist
       `((".*" . "~/.emacs.d/backups/")))
+
+;; Enable a color theme
+(load-theme 'modus-operandi) ;; Light mode
+;; (load-theme 'modus-vivendi) ;; Dark mode
 
 ;; Don't show the startup screen
 (setq inhibit-startup-message t)
@@ -25,8 +23,44 @@
 (global-hl-line-mode t)
 
 ;; Overwrite selected text
-;; Source: http://pragmaticemacs.com/emacs/overwrite-selected-text/
 (delete-selection-mode t)
 
-;; Add line numbers in programming modes
+;; Display line numbers in programming modes
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+(defun system-has-gls ()
+  "Check if the system has 'gls' installed."
+  (stringp (shell-command-to-string "command -v gls")))
+
+(when (eq system-type 'darwin) ;; Check if on macOS (Darwin)
+  (if (system-has-gls)
+      ;; If 'gls' is available, use it for dired listing
+      (setq insert-directory-program "gls"
+            dired-listing-switches "-al --group-directories-first")
+    ;; If 'gls' is not available, disable 'ls-lisp' for dired listing
+    (setq dired-use-ls-dired nil)))
+
+;; PACKAGE INSTALLATION SETUP
+;; Source: https://stackoverflow.com/questions/55038594/setting-up-emacs-on-new-machine-with-init-el-and-package-installation
+
+;; Declare package repos
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")))
+
+;; Initialize package.el
+(require 'package)
+(package-initialize)
+(package-refresh-contents) ;; After first run, comment out this line
+
+;; Declare packages
+(setq my-packages
+      '(edit-indirect
+	groovy-mode
+	markdown-mode
+	web-mode))
+
+;; Install packages
+(dolist (pkg my-packages)
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
